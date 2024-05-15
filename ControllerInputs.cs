fa24a519bfefaa495c7754079d5fbb0f55ce3fdc
Shade949 <emasterson206@icloud.com>
2024-05-15 15:47:31 -0500

    Add files via upload

---
+++ ControllerInputs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.XR;
+using InputDevice = UnityEngine.XR.InputDevice;
+
+public class ControllerInputs : MonoBehaviour
+{
+    private void Awake() {}
+    private void Start() 
+    { 
+    
+    }
+
+    // Updates are held once per frame
+    private void Update() // This is where the inputs will be held
+    {
+        List<InputDevice> m_device = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right,m_device);
+        if (m_device.Count == 1) 
+        {
+            // One Device Found
+            CheckController(m_device[0]);
+        }
+        else
+        {
+            
+        }
+
+    }
+
+    private void CheckController(InputDevice d) 
+    {
+        bool primaryButtonDown = false;
+        d.TryGetFeatureValue(CommonUsages.primaryButton ,out primaryButtonDown);
+        if (primaryButtonDown)
+        {
+
+        }
+        else 
+        {
+
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.shade.gorillatag.menutemplate";
+        public const string Name = "Shadow Pad";
+        public const string Description = "Created by @shade949";
+        public const string Version = "1.0.0";
+    }
+}
